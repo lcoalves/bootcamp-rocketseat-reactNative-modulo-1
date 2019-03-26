1dbc7da8fafc010eb5a96ea71770888d21ab4182
Lucas Alves <lucas.alves@udf.org.br>
2019-03-26 14:19:54 -0300

    Primeiro commit react-native modulo1

---
+++ App.js
@@ -0,0 +1,19 @@
+import './src/config/ReactotronConfig';
+
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FC0',
+  },
+});
